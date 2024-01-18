c126cf55bc4e4916f6e3d5641ba5d4158c5f97b7
Epoza <86025026+Epoza@users.noreply.github.com>
2024-01-17 22:05:44 -0500

    add modal overlay for task and  category

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
 import './style.css'
+import { modal } from "./modal.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>ToDo List App</h1>
-  </div>
-`
+// handle click events
+document.body.addEventListener('click', (event: MouseEvent) => {
+  const target = event.target as HTMLElement;
+  
+  // Code for modal
+  if (target.classList.contains('showModal') || (target.parentElement && target.parentElement.classList.contains('showModal'))) {
+    // Get the ID of the clicked button
+    const buttonId = target.id || (target.parentElement && target.parentElement.id);
+
+    // Perform different actions based on the button ID
+    switch (buttonId) {
+      case 'category': // newCategory button clicked
+        modal(buttonId)
+        console.log('Clicked newCategory button');
+        break;
+      case 'task': // newTask button clicked
+        modal(buttonId)
+        console.log('Clicked newTask button');
+        break;
+      // Add more cases as needed
+    }
+  }
+});
---
+++ src/modal.ts
@@ -0,0 +1,69 @@
+import './style.css'
+
+export function modal(contentType: string) {
+    // Capitilize first letter
+    const capitalizedContentType = contentType.charAt(0).toUpperCase() + contentType.slice(1);
+
+    // for task and category
+    let getModal = document.querySelector<HTMLDivElement>('#modal-container');
+    // generate different modal depending on which button was clicked
+    if(contentType == 'task'){
+        getModal!.innerHTML = 
+        `
+        <div id="modal-content">
+         <div id="modalHeader">
+          <h2>Create New ${capitalizedContentType}</h2>
+          <div class="svgButton" id="close"><img src="../images/close.svg" alt="close modal icon"></div>
+         </div>
+         <form id="newForm">
+          <label for="${contentType}Name">${capitalizedContentType} Name*</label>
+          <input type="text" id="${contentType}Name" name="${contentType}Name" placeholder="Clean room" required>
+          <label for="${contentType}Description">${capitalizedContentType} Description:</label>
+          <textarea id="${contentType}Description" name="${contentType}Description" rows="4" placeholder="Optional"></textarea>
+          <div class="modal${capitalizedContentType}Buttons">
+           <button class="modal-button" id="cancel">Cancel</button>
+           <button class="modal-button" type="submit">Create ${capitalizedContentType}</button>
+          </div>
+         </form>
+        </div>
+        `
+        // submit contents of form to new script and append to screen
+    } else if (contentType == 'category'){
+        getModal!.innerHTML = 
+        `
+        <div id="modal-content">
+         <div id="modalHeader">
+          <h2>Create New ${capitalizedContentType}</h2>
+          <div class="svgButton" id="close"><img src="../images/close.svg" alt="close modal icon"></div>
+         </div>
+         <form id="newForm">
+          <label for="${contentType}Name">${capitalizedContentType} Name*</label>
+          <input type="text" id="${contentType}Name" name="${contentType}Name" required>
+          <div class="modal${capitalizedContentType}buttons">
+           <button class="modal-button" id="cancel">Cancel</button>
+           <button class="modal-button" type="submit">Create ${capitalizedContentType}</button>
+          </div>
+         </form>
+        </div>
+        `
+    }
+    toggleModal()
+    // show/hide modal
+    function toggleModal() {
+        var modalContainer = document.getElementById('modal-container');
+        if (modalContainer?.style.display === 'flex') {
+            modalContainer.style.display = 'none';
+        } else {
+            modalContainer!.style.display = 'flex';
+        }
+    }
+    document.getElementById('cancel')!.addEventListener('click', function (event) {
+        event.preventDefault();
+        toggleModal();
+    });
+    document.getElementById('close')!.addEventListener('click', function (event) {
+        event.preventDefault();
+        toggleModal();
+    });
+
+}
