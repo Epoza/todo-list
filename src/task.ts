55c0f03d7908df218a7c0b91cb3b24d1116bbd35
Epoza <86025026+Epoza@users.noreply.github.com>
2024-01-31 23:05:15 -0500

    test createTask function

--- src/task.ts
+++ src/task.ts
@@ -12,11 +12,37 @@ export const tasks = (() => {
     let tasksList: Task[] = [];
     
     function createTask(name: string, description?: string): void {
-        // get the currently selected category and append the data to it
-        const newTask = new Task(name, description);
-        tasksList.push(newTask);
-        console.log(tasksList)
-        console.log(newTask.name + ' ' + newTask.description);
+        // get the currently selected category
+        const selectedCategory = document.querySelector('.myCategories.categorySelected');
+
+        if (selectedCategory) {
+            // extract the category index from the data-category attribute
+            const categoryIndex = selectedCategory.getAttribute('data-category');
+
+            // create a new task with the associated category index
+            const newTask = new Task(name, description);
+
+            // Test categpry index
+            console.log(`Task created for category index ${categoryIndex}: ${newTask.name} ${newTask.description}`);
+
+            tasksList.push(newTask);
+
+            // Check if tasksList has entries, Maybe check if the slected category has task entries instead
+            if (tasksList.length > 0) {
+                // Find the element using its ID
+                const addSvgButton = document.getElementById('task');
+                // Check if the element exists
+                if (addSvgButton) {
+                    // restyle the button
+                    addSvgButton.classList.remove(".noTasks");
+                    addSvgButton.classList.add('hasTasks');
+                    addSvgButton.style.width = '24px'; // Adjust the width
+                    addSvgButton.style.height = '24px'; // Adjust the height
+                }
+            }
+        } else {
+            console.error('No category selected for the task.');
+        }
     }
 
     function removeTask(event: Event): void {
