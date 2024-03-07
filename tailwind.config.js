4c6040e62a6cf7c695f4bed43964e1d45697176a
Epoza <86025026+Epoza@users.noreply.github.com>
2024-03-06 23:03:44 -0500

    add darkmode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,27 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         NotoSans: ["Noto Sans", "sans-serif"],
+      },
+      light: {
+        // change these later
+        background: ' ',
+        foreground: ' ',
+        mainText: ' ',
+        subText: ' ',
+        border: ' ',
+      },
+      dark: {
+        // change these later
+        background: ' ',
+        foreground: ' ',
+        mainText: ' ',
+        subText: ' ',
+        border: ' ',
       }
     },
   },
