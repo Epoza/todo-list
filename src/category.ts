8c5b601e6e6aa1409855f29897528e53a7e71c9a
Epoza <86025026+Epoza@users.noreply.github.com>
2024-03-13 23:20:43 -0400

    change edit & remove to use svg's directly

--- src/category.ts
+++ src/category.ts
@@ -95,20 +95,16 @@ export const categories = (() => {
             // edit svg button
             const categoryEdit = document.createElement('div');
             categoryEdit.classList.add('svgButton');
-            const editIcon = document.createElement('img');
-            editIcon.src = "../images/edit.svg"
-            editIcon.alt = 'edit category icon';
-            categoryEdit.appendChild(editIcon);
+            categoryEdit.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>';
             svgButtonContainer.appendChild(categoryEdit);
 
             // remove svg button
             const categoryRemove = document.createElement('div');
             categoryRemove.classList.add('svgButton');
             categoryRemove.setAttribute('data-remove', categoryIndex.toString());
-            const removeIcon = document.createElement('img');
-            removeIcon.src = "../images/remove.svg"
-            removeIcon.alt = 'remove category icon';
-            categoryRemove.appendChild(removeIcon);
+            categoryRemove.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"/></svg>';
+            
+            svgButtonContainer.appendChild(categoryRemove);
             // Add click event listener directly to categoryRemove
             categoryRemove.addEventListener('click', (event) => {
                 // Prevent the click event from propagating to the parent elements
