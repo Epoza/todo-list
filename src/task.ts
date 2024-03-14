63e9fd195d88d0c8da4385f9fad152e0b26dc412
Epoza <86025026+Epoza@users.noreply.github.com>
2024-03-14 18:46:11 -0400

    add SVG's & toggle between checked states

--- src/task.ts
+++ src/task.ts
@@ -7,15 +7,18 @@ export class Task {
     important: boolean;
     date?: string;
     description?: string;
+    checked: boolean;
 
     // Properties to track default categories
     taskInCategory0: boolean = false;
     taskInCategory1: boolean = false;
     taskInCategory2: boolean = false;
+    
 
-    constructor(name: string, categoryIndex: string, important: boolean, date?: string, description?: string) {
+    constructor(name: string, categoryIndex: string, checked: boolean, important: boolean, date?: string, description?: string) {
         this.name = name;
         this.categoryIndex = categoryIndex;
+        this.checked = checked
         this.important = important;
         this.date = date;
         this.description = description;
@@ -25,8 +28,21 @@ export class Task {
 export const tasks = (() => {
     let tasksList: Task[] = [];
 
+    // SVG's
+    const uncheckedBoxSvg = '<svg class="toggleCheckedBox taskButtonClick unchecked" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Z"/></svg>'
+
+    const checkedBoxSvg = '<svg class="toggleCheckedBox taskButtonClick checked" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg>'
+
+    const descriptionSvg = '<svg class="taskButtonClick" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-80 240-320l57-57 183 183 183-183 57 57L480-80ZM298-584l-58-56 240-240 240 240-58 56-182-182-182 182Z"/></svg>'
+
+    const editSvg = '<svg class="taskButtonClick" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>'
+
+    const removeSvg = '<svg class="taskButtonClick" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"/></svg>'
+
+    const mainCategorySvg = '<svg class="taskButtonClick" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M240-200h133.847v-237.692h212.306V-200H720v-360L480-740.769 240-560v360Zm-59.999 59.999v-449.998L480-815.767l299.999 225.768v449.998H526.154v-237.693h-92.308v237.693H180.001ZM480-470.385Z"/></svg>'
+
     function addTaskToList(name: string, categoryIndex: string, important: boolean, date?: string, description?: string): void {
-        const newTask = new Task(name, categoryIndex!, important, date, description);
+        const newTask = new Task(name, categoryIndex!, false, important, date, description);
         tasksList.push(newTask);
         createTask(newTask);
         saveTasksList();
@@ -81,12 +97,8 @@ export const tasks = (() => {
         const uncheckedTask = document.createElement('div');
         uncheckedTask.classList.add('svgButton', 'taskButton');
         uncheckedTask.id = 'checkButton';
-        const uncheckedTaskIcon = document.createElement('img');
-        uncheckedTaskIcon.classList.add("taskButtonClick");
-        uncheckedTaskIcon.src = "../images/uncheckedBox.svg";
-        uncheckedTaskIcon.alt = 'unchecked task icon';
-        uncheckedTaskIcon.id = 'unchecked';
-        uncheckedTask.appendChild(uncheckedTaskIcon);
+        console.log(currentTask.checked)
+        uncheckedTask.innerHTML = currentTask.checked ? checkedBoxSvg : uncheckedBoxSvg;
         taskContent.appendChild(uncheckedTask);
 
         // button svg holder
@@ -97,11 +109,7 @@ export const tasks = (() => {
         const descriptionButton = document.createElement('div');
         descriptionButton.classList.add('svgButton', 'taskButton');
         descriptionButton.id = 'descriptionButton';
-        const descriptionIcon = document.createElement('img');
-        descriptionIcon.classList.add("taskButtonClick");
-        descriptionIcon.src = "../images/description.svg";
-        descriptionIcon.alt = 'description dropdown icon';
-        descriptionButton.appendChild(descriptionIcon);
+        descriptionButton.innerHTML = descriptionSvg;
         taskSvgButtonContainer.appendChild(descriptionButton)
         
         if(!defaultCategory){
@@ -109,11 +117,7 @@ export const tasks = (() => {
             const taskEdit = document.createElement('div');
             taskEdit.classList.add('svgButton', 'taskButton');
             taskEdit.id = 'editButton';
-            const taskEditIcon = document.createElement('img');
-            taskEditIcon.classList.add("taskButtonClick");
-            taskEditIcon.src = "../images/edit.svg";
-            taskEditIcon.alt = 'edit task icon';
-            taskEdit.appendChild(taskEditIcon);
+            taskEdit.innerHTML = editSvg;
             taskSvgButtonContainer.appendChild(taskEdit);
 
             // delete button
@@ -122,23 +126,14 @@ export const tasks = (() => {
             taskRemove.id = 'removeButton';
             // possibly change to just data-remove
             taskRemove.setAttribute('data-remove-task', taskIndex.toString());
-            
-            const taskRemoveIcon = document.createElement('img');
-            taskRemoveIcon.classList.add("taskButtonClick");
-            taskRemoveIcon.src = "../images/remove.svg";
-            taskRemoveIcon.alt = 'unchecked task icon';
-            taskRemove.appendChild(taskRemoveIcon);
+            taskRemove.innerHTML = removeSvg;
             taskSvgButtonContainer.appendChild(taskRemove);
         } else {
             // goes to category of main task
             const mainCategoryButton = document.createElement('div');
             mainCategoryButton.classList.add('svgButton', 'taskButton');
             mainCategoryButton.id = 'mainCategoryButton';
-            const mainCategoryIcon = document.createElement('img');
-            mainCategoryIcon.classList.add("taskButtonClick");
-            mainCategoryIcon.src = "../images/main_category.svg";
-            mainCategoryIcon.alt = 'main category icon';
-            mainCategoryButton.appendChild(mainCategoryIcon);
+            mainCategoryButton.innerHTML = mainCategorySvg;
             taskSvgButtonContainer.appendChild(mainCategoryButton)
         }
         
@@ -210,7 +205,6 @@ export const tasks = (() => {
             createTask(currentTask, '2');
             currentTask.taskInCategory2 = true;
         }
-    
         updateTasks(currentTask.categoryIndex);
     }
 
@@ -264,40 +258,9 @@ export const tasks = (() => {
 
             switch (buttonId) {
                 case ButtonId.checkButton:
-                    // toggle between unchecked and checked
-                    const checkTaskIcon = button.querySelector('img') as HTMLImageElement;
-                    if (checkTaskIcon) {
-                        const currentIcon = checkTaskIcon.src;
-                        const toggleIcon = currentIcon.includes('uncheckedBox.svg')
-                            ? "../images/checkedBox.svg"
-                            : "../images/uncheckedBox.svg";
-                        checkTaskIcon.src = toggleIcon;
-                        checkTaskIcon.id = checkTaskIcon.id === 'unchecked' ? 'checked' : 'unchecked';
-                        checkTaskIcon.alt = checkTaskIcon.alt === 'unchecked task icon' ? 'checked task icon' : 'unchecked task icon';
-
-                        // Change styling for all tasks with the same data-task
-                        const dataTask = checkTaskIcon.closest('.myTask')?.getAttribute('data-task');
-                        if (dataTask) {
-                            const tasksWithSameDataTask = document.querySelectorAll(`.myTask[data-task="${dataTask}"]`);
-                            tasksWithSameDataTask.forEach(taskElement => {
-                                const taskText = taskElement?.querySelectorAll('span');
-                                if (taskText) {
-                                    taskText.forEach(spanElement => {
-                                        spanElement.style.textDecoration = checkTaskIcon.id === 'checked' ? 'line-through' : 'currentColor';
-                                    });
-                                    // set the new taskIcon
-                                    const taskIcon = taskElement.querySelector('img') as HTMLImageElement;
-                                    if (taskIcon) {
-                                        taskIcon.src = toggleIcon;
-                                        taskIcon.id = checkTaskIcon.id;
-                                        taskIcon.alt = checkTaskIcon.alt;
-                                    }
-
-                                    (taskElement as HTMLElement).style.backgroundColor = checkTaskIcon.id === 'checked' ? 'rgba(0, 0, 0, 0.3)' : '';
-                                }
-                            });
-                        }
-                    }
+                    const checkTaskIcon = button.querySelector('svg') as SVGElement;
+                    console.log(checkTaskIcon)
+                    changeCheckedState(currentTask, true);
                     break;
                 case ButtonId.DescriptionButton:
                     // Toggle the display of the description dropdown
@@ -327,6 +290,39 @@ export const tasks = (() => {
                     break;
             }
         }
+    }
+
+    function changeCheckedState(currentTask: Task, clicked = false){
+        // If clicked then toggle
+        if(clicked){
+            currentTask.checked = !currentTask.checked;  
+            const isChecked = currentTask.checked;
+            // Toggle the SVG for all tasks with the same data-task
+            const taskIndex = tasksList.indexOf(currentTask);
+            const tasksWithSameIndex = document.querySelectorAll(`.myTask[data-task="${taskIndex}"]`);
+            tasksWithSameIndex.forEach(taskElement => {
+                const taskIcon = taskElement.querySelector('svg') as SVGElement;
+                if (taskIcon) {
+                    const toggleIcon = isChecked ? checkedBoxSvg : uncheckedBoxSvg;
+                    taskIcon.outerHTML = toggleIcon;
+                }
+            });      
+            
+        }
+        // change the styling depending on if checked or not
+        const taskIndex = tasksList.indexOf(currentTask);
+        const tasksWithSameIndex = document.querySelectorAll(`.myTask[data-task="${taskIndex}"]`);
+        tasksWithSameIndex.forEach(taskElement => {
+            const taskText = taskElement?.querySelectorAll('span');
+            if (taskText) {
+                taskText.forEach(spanElement => {
+                    spanElement.style.textDecoration = currentTask.checked ? 'line-through' : 'currentColor'
+                });
+                // set the new taskIcon
+                (taskElement as HTMLElement).style.backgroundColor = currentTask.checked ? 'rgba(0, 0, 0, 0.3)' : '';
+            } 
+        });
+        saveTasksList();
     }
 
     function removeTask(currentTask: Task): void {
@@ -520,17 +516,20 @@ export const tasks = (() => {
         if (storedTasks) {
             const storedTasksParsed: Task[] = JSON.parse(storedTasks);
             tasksList = storedTasksParsed.map(taskData => {
-                return new Task(taskData.name, taskData.categoryIndex, taskData.important, taskData.date, taskData.description);
+                return new Task(taskData.name, taskData.categoryIndex, taskData.checked, taskData.important, taskData.date, taskData.description);
             });
             tasksList.forEach((currentTask) => {
                 createTask(currentTask);
                 // Update UI after creating each task
                 updateTasks(currentTask.categoryIndex);
+                // Toggle checked state if necessary
+                if(currentTask.checked){
+                    changeCheckedState(currentTask);
+                }
             });
         }
     }
     
-
     // Check if tasksList exists in localStorage and retrieve it
     const storedTasks = localStorage.getItem('tasksList');
     if (storedTasks) {
